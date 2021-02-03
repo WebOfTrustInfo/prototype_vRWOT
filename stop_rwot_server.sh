e983b533b870c3020119e101a6351e9a6da3624a
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-02-03 15:35:25 +0000

    Add a reminder that DGD can auto-restart to stop_rwot_server.sh

--- stop_rwot_server.sh
+++ stop_rwot_server.sh
@@ -11,3 +11,9 @@ else
     echo "DGD is running with PID ${DGD_PID}. Stopping."
     kill "$DGD_PID"
 fi
+
+cat <<EndOfMessage
+Remember that DGD may be configured to automatically restart via Cron or similar!
+If you want to prevent the start script from restarting DGD, you may want to
+touch the file no_restart.txt in the appropriate application directory.
+EndOfMessage
