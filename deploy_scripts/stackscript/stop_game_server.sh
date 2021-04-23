ccbc7244e49f08ec1657bdd4177bc39520021528
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-04-23 13:08:15 +0100

    Don't fail Linode stop_game_server script if DGD wasn't running

--- deploy_scripts/stackscript/stop_game_server.sh
+++ deploy_scripts/stackscript/stop_game_server.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-DGD_PID=$(pgrep -f "/var/dgd/bin/dgd dgd.config")
+DGD_PID=$(pgrep -f "/var/dgd/bin/dgd dgd.config") || echo "DGD wasn't running, good"
 if [ -z "$DGD_PID" ]
 then
     echo "DGD does not appear to be running. Good."
